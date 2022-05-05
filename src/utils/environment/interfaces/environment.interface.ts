05794f59e6db8d3c3ad808db7c17b303616ac8b4
WhoKnows <who@knows.eus>
2022-05-06 00:15:39 +0200

    Create nestjs boilerplate with environment and neo4j connection

--- src/utils/environment/interfaces/environment.interface.ts
+++ src/utils/environment/interfaces/environment.interface.ts
@@ -5,6 +5,9 @@ export interface CustomConfigOptions {
 // The private and public keys will be encoded in Base64
 export interface EnvironmentConfigParams {
   ddbb: {
+    scheme: string;
+    host: string;
+    port: number;
     name: string;
     username: string;
     password: string;
